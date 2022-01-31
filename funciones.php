8e0869f7def20e63c6ddf71d0fce04a686f016f5
magiksen <elemejore@gmail.com>
2022-01-31 10:48:43 -0600

    UTF8 correctamente

--- funciones.php
+++ funciones.php
@@ -2,7 +2,7 @@
 
 function conexion($bd_config){
     try {
-        $conexion = new PDO('mysql:host=localhost;dbname='.$bd_config['basedatos'], $bd_config['usuario'], $bd_config['pass']);
+        $conexion = new PDO('mysql:host=localhost;charset=utf8;dbname='.$bd_config['basedatos'], $bd_config['usuario'], $bd_config['pass']);
         return $conexion;
     } catch (PDOException $e) {
         return false;
